c34af23de52ccfee330f9f2c6602eeba66042069
cpshan <cody.33shan@gmail.com>
2022-05-16 10:00:44 -0700

    Augmentation update

--- augmentData.py
+++ augmentData.py
@@ -4,10 +4,10 @@ from pathlib import Path
 
 normal_dir = Path('./chest-xray-pneumonia/chest_xray/train/NORMAL')
 
-for filename in os.scandir(normal_dir):
-    img = Image.open(normal_dir + filename)
-    img_mirror = ImageOps.mirrored(img)
-    img_mirror.save(normal_dir + filename + '_mirrored')
+for filename in os.listdir(normal_dir):
+    img = Image.open(normal_dir / filename)
+    img_mirror = ImageOps.mirror(img)
+    img_mirror.save(normal_dir / ('_mirrored' + filename))
 
 
 
